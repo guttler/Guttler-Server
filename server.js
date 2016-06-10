b39afb585f39d71b3a6b4b1d0265c831ceaf9193
Mark Wen <markwenguojie94@gmail.com>
2016-06-10 01:06:54 -0400

    06/10/2016
    Changed to Linter style code
    Working on connecting to firebase database

--- server.js
+++ server.js
@@ -1,49 +1,47 @@
-/**************************** Guttler Main Server Script ****************************/
-/************************************************************************************/
+/** ************************** Guttler Main Server Script ************************** **/
+/** ******************************************************************************** **/
 
-
-/******************************** Base Package Setup ********************************/
-var express = require('express');
-var app = express();
-var bodyParser = require("body-parser");
-var firebase = require("firebase");
-
+/** ****************************** Base Package Setup ****************************** **/
+var express = require('express')
+var app = express()
+var bodyParser = require('body-parser')
+var firebase = require('firebase')
 // Setting firebase credentials and database address
 firebase.initializeApp({
-  serviceAccount: "./serviceAccountCredentials.json",
-  databaseURL: "https://guttler-server.firebaseio.com/"
-});
+  serviceAccount: './serviceAccountCredentials.json',
+  databaseURL: 'https://guttler-server.firebaseio.com/'
+})
+// Connecting to firebase database
+var db = firebase.database()
+var ref = db.ref('/Place')  // Figure this line out...
+ref.once('value', function (snapshot) {
+  console.log(snapshot.val())
+})
+// Configure app to use bodyParser(), which will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
-// configure app to use bodyParser(), which will let us get the data from a POST
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-
-/*************************************** APIs ***************************************/
+/** ************************************* APIs ************************************* **/
 // Routes for api
-var router = express.Router();
-
+var router = express.Router()
 // Middleware to verify token(to be implemented...), it will be called everytime a request is sent to API
-router.use(function(req, res, next) {
-    console.log('A request is coming in.');
-    next();
-});
-
+router.use(function (req, res, next) {
+  console.log('A request is coming in.')
+  next()
+})
 // Test route to make sure APIs are working
-router.get('/', function(req, res) {
-    res.json({ message: 'Guttler-Server APIs are running.' });
-});
-
+router.get('/', function (req, res) {
+  res.json({ message: 'Guttler-Server APIs are running.' })
+})
 // Registering out routes, all of our routes will be prefixed with /api
-app.use('/', router);
+app.use('/', router)
 
-
-/********************************** Server Running **********************************/
-var port = process.env.PORT || 3000;
-app.listen(port, function(err) {
+/** ******************************** Server Running ******************************** **/
+var port = process.env.PORT || 3000
+app.listen(port, function (err) {
   if (err) {
-    console.log(err);
+    console.log(err)
   } else {
-    console.log("Listening on port " + port + ".");
+    console.log('Listening on port ' + port + '.')
   }
-});
+})
