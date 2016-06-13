ff76c5731b1f33b21b89cf9525abc94f702c023c
Mark Wen <markwenguojie94@gmail.com>
2016-06-13 01:11:36 -0400

    06/13/2016
    Added an API for adding places to firebase db

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-/** ************************** Guttler Main Server Script ************************** **/
+/** ***************************** Guttler Server Script **************************** **/
 /** ******************************************************************************** **/
 
 /** ****************************** Base Package Setup ****************************** **/
@@ -11,34 +11,55 @@ firebase.initializeApp({
   serviceAccount: './serviceAccountCredentials.json',
   databaseURL: 'https://guttler-server.firebaseio.com/'
 })
-// Connecting to firebase database
+// Connecting to firebase database and get the reference of each data object type
 var db = firebase.database()
-var ref = db.ref('/Place')  // Figure this line out...
-ref.once('value', function (snapshot) {
-  console.log(snapshot.val())
-})
+var placeRef = db.ref('/Places')
 // Configure app to use bodyParser(), which will let us get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 /** ************************************* APIs ************************************* **/
-// Routes for api
-var router = express.Router()
-// Middleware to verify token(to be implemented...), it will be called everytime a request is sent to API
-router.use(function (req, res, next) {
+// Route for api
+var api = express.Router()
+// TODO: Middleware to verify token, it will be called everytime a request is sent to API
+api.use(function (req, res, next) {
   console.log('A request is coming in.')
   next()
 })
 // Test route to make sure APIs are working
-router.get('/', function (req, res) {
+api.get('/', (req, res)=> {
   res.json({ message: 'Guttler-Server APIs are running.' })
 })
+// API for adding an instance to "Places" table
+// TODO: Figure out how to add Chinese characters
+api.post('/placesAdd', (req, res)=> {
+  placeRef.push({
+    name: req.headers.name,
+    category: req.headers.category,
+    subcategory: req.headers.subcategory,
+    address: req.headers.address,
+    city: req.headers.city,
+    state: req.headers.state,
+    zip: req.headers.zip,
+    phone: req.headers.phone,
+    website: req.headers.website,
+    description: req.headers.description
+  })
+  res.json({ message: 'Place added' })
+})
+// TODO: API for universal search
+// First retrieve all the objects from firebase
+// Next try to search all of the objects based on the keywords
+// And sort of the first 20 objects by distance from the appointed geolocation
+api.get('/placesUniversalSearch'), (req, res)=> {
+
+}
 // Registering out routes, all of our routes will be prefixed with /api
-app.use('/', router)
+app.use('/', api)
 
 /** ******************************** Server Running ******************************** **/
 var port = process.env.PORT || 3000
-app.listen(port, function (err) {
+app.listen(port, err=> {
   if (err) {
     console.log(err)
   } else {
