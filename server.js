eac9fcc42786a74115c08640cec0dcf0c2ef2c1c
Mark Wen <markwenguojie94@gmail.com>
2016-06-14 00:36:14 -0400

    06/14/2016
    50% done with placesUniversalSearch() API

--- server.js
+++ server.js
@@ -18,6 +18,46 @@ var placeRef = db.ref('/Places')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
+/** ******************************* Global Functions ******************************* **/
+// Function for comparing 2 json objects, used in itemExists()
+function compareObjects(o1, o2) {
+  var k = ''
+  for (k in o1) {
+    if (o1[k] != o2[k]) {
+      return false
+    }
+  }
+  for (k in o2) {
+    if(o1[k] != o2[k]) {
+      return false
+    }
+  }
+  return true
+}
+// Function for checking if an item exists, used in searchFor()
+function itemExists(haystack, needle) {
+  for (var i = 0; i < haystack.length; i++) {
+    if (compareObjects(haystack[i], needle)) {
+      return true
+    }
+  }
+  return false
+}
+// Function for searching a specific term in a json object, used in api.post('/placesUniversalSearch')
+function searchFor(term, allData) {
+  var results = []
+  for (var i = 0; i < allData.length; i++) {
+    for (var key in allData[i]) {
+      if (allData[i][key].indexOf(term)!=-1) {
+        if (!itemExists(results, allData[i])) {
+          results.push(allData[i])
+        }
+      }
+    }
+  }
+  return results
+}
+
 /** ************************************* APIs ************************************* **/
 // Route for api
 var api = express.Router()
@@ -30,8 +70,7 @@ api.use(function (req, res, next) {
 api.get('/', (req, res)=> {
   res.json({ message: 'Guttler-Server APIs are running.' })
 })
-// API for adding an instance to "Places" table
-// TODO: Figure out how to add Chinese characters
+// API for adding an instance to "Places" table, created for testing purposes, don't use it for client
 api.post('/placesAdd', (req, res)=> {
   placeRef.push({
     name: req.body.name,
@@ -47,13 +86,23 @@ api.post('/placesAdd', (req, res)=> {
   })
   res.json({ message: 'Place added' })
 })
-// TODO: API for universal search
-// First retrieve all the objects from firebase
-// Next try to search all of the objects based on the keywords
-// And sort of the first 20 objects by distance from the appointed geolocation
-api.get('/placesUniversalSearch'), (req, res)=> {
-
-}
+// API for universal search
+api.post('/placesUniversalSearch', (req, res)=> {
+  var allData, allResults, results
+  const term = req.body.term.trim()
+  // First retrieve all the objects from firebase
+  placeRef.on("value", snapshot=> {
+    allData = snapshot.val()
+  }, errorObject=> {
+    console.log("The read failed: " + errorObject.code)
+  })
+  // Then change the object into normal json format
+  // TODO: Implement the step
+  // Next try to search all of the objects based on the term
+  allResults = searchFor(term, allData)
+  // And sort of the first 20 objects by distance from the appointed geolocation
+  // TODO: Implement the step
+})
 // Registering out routes, all of our routes will be prefixed with /api
 app.use('/', api)
 
